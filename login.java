64ae61152e09670b2201db6a515dd9d354f9d0ca
bhargav <bhargavsaikumar03@gmail.com>
2021-06-13 12:57:11 -0400

    Initial commit

--- login.java
+++ login.java
@@ -1,12 +1,9 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-hi
-how are you?
-
+ <<<<<<< HEAD <<<<<<< HEAD hi how are you?
+hi bhargav
 =======
 bhargav is doing good 
 =======
-
+hey devops
 
 gresham place 600, bhargav is doing good 
 >>>>>>> demo
